200c82c0af183ee396dce7aa4b58f4a54b306ebf
thaibaocoder1 <thaibaok4.1@gmail.com>
2024-01-01 22:16:52 +0700

    1/1/24: continue finish code

--- ex-33-notes-app/main.js
+++ ex-33-notes-app/main.js
@@ -1,5 +1,8 @@
+import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
+const notes = JSON.parse(localStorage.getItem("notes"));
+notes && notes.forEach((note) => addNewNote(note));
 const addBtn = document.getElementById("add");
-addBtn.addEventListener("click", () => addNewNote("Hello World"));
+addBtn.addEventListener("click", () => addNewNote());
 function addNewNote(text = "") {
   const note = document.createElement("div");
   note.classList.add("note");
@@ -18,13 +21,25 @@ function addNewNote(text = "") {
   const main = note.querySelector(".main");
   const textarea = note.querySelector("textarea");
   textarea.value = text;
-  main.innerHTML = text;
+  main.innerHTML = marked.parse(text);
   deleteBtn.addEventListener("click", () => {
     note.remove();
+    updateLS();
   });
   editBtn.addEventListener("click", () => {
     main.classList.toggle("hidden");
     textarea.classList.toggle("hidden");
   });
+  textarea.addEventListener("input", function (e) {
+    const { value } = e.target;
+    main.innerHTML = marked.parse(value);
+    updateLS();
+  });
   document.body.appendChild(note);
+}
+function updateLS() {
+  const notesText = document.querySelectorAll("textarea");
+  const notes = [];
+  notesText.forEach((note) => notes.push(note.value));
+  localStorage.setItem("notes", JSON.stringify(notes));
 }
